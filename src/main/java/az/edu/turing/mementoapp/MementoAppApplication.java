a67b11e41d6ed1c34f673cc37a6a5a5cba5adf1b
HP ProBook <kalbaliyevaselen@gmai.com>
2024-08-09 16:50:52 +0400

    first commit

---
+++ src/main/java/az/edu/turing/mementoapp/MementoAppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.mementoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MementoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/edu/turing/mementoapp/MementoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.mementoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
